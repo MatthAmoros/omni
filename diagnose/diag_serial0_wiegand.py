cb635a7152ebf6519bb1842ea66d4ce437b5d993
SunPaz <matth.amoros@gmail.com>
2018-05-18 12:48:19 -0700

    Yeeep

--- diagnose/diag_serial0_wiegand.py
+++ diagnose/diag_serial0_wiegand.py
@@ -15,13 +15,13 @@ bits=26 value=12442091
 """
 
 if __name__ == "__main__":
-   import time
+    import time
 
 
-   def callback(bits, value):
-      print("bits={} value={}".format(bits, value))
+    def callback(bits, value):
+        print("bits={} value={}".format(bits, value))
 
-  with WiegandReader(GPIO, 14, 15, callback) as w:
-      while True:
-          time.sleep(300)
-      w.cancel()
+    with WiegandReader(GPIO, 14, 15, callback) as w:
+        while True:
+            time.sleep(300)
+        w.cancel()
