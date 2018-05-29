7ba978953862cfa6467971ad2ff50049d0946212
SunPaz <matth.amoros@gmail.com>
2018-05-29 14:25:31 -0700

    Changed configuration file's path from relative to absolute path (for deployment)

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ from lib.sourceFactory import SourceFactory
 from lib.visibilityManager import VisibilityManager
 from lib.common import ServerSetting, DeviceConfiguration, Member
 
-CONNECTION_FILE_PATH = "./cfg/connectionString.sql" #Default
+CONNECTION_FILE_PATH = "/app/omni/cfg/connectionString.sql" #Default
 SERVER_SECRET = "DaSecretVectorUsedToHashCardId" #Default
 
 connected_devices = []
