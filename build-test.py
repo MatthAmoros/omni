a15950d7b811dcd377ee8f2e7aec8ae6a9d1c3bf
SunPaz <matth.amoros@gmail.com>
2018-05-30 10:24:36 -0700

    Removed uninstalled packages

--- build-test.py
+++ build-test.py
@@ -1,27 +1,20 @@
 """ This file is used by Travis to test code """
-from .server import *
-from .clientv2 import *
+from server import *
+from client import *
 from lib.sourceFactory import *
 from lib.common import *
 from lib.devices.deviceBase import *
 
 import os
-import flaskr
 import unittest
 import tempfile
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.testing = True
-        self.app = flaskr.app.test_client()
-        with flaskr.app.app_context():
-            flaskr.init_db()
-
+        print("Setting up tests")
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        print("Tearing down tests")
 
 if __name__ == '__main__':
     unittest.main()
