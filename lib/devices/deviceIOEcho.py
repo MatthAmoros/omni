dc9f1419418b01ddc0ab138cbd12ce5c4b638a27
SunPaz <matth.amoros@gmail.com>
2019-09-06 13:17:24 -0700

    Added pin numbering mode and basic configuration

--- lib/devices/deviceIOEcho.py
+++ lib/devices/deviceIOEcho.py
@@ -21,27 +21,29 @@ except RuntimeError:
 class IOEcho(DeviceBase):
 	def __init__(self, name, pin_and_label_matrix):
 		DeviceBase.__init__(self, name)
+		print("Starting IOEcho device.")
+
+		"""
+			Set pin numbering mode
+		"""
+		GPIO.setmode(GPIO.BCM)
 
 		self.pin_and_label_matrix = [
-			{'pin': 1, 'label': 'S011'},
-			{'pin': 2, 'label': 'S012'},
-			{'pin': 3, 'label': 'S033'},
-			{'pin': 4, 'label': 'S021'},
-			{'pin': 5, 'label': 'S022'},
-			{'pin': 6, 'label': 'S023'},
-			{'pin': 7, 'label': 'S031'},
-			{'pin': 8, 'label': 'S032'},
-			{'pin': 9, 'label': 'S033'}
+			{'pin': 2, 'label': 'S011'},
+			{'pin': 3, 'label': 'S012'},
+			{'pin': 4, 'label': 'S033'},
+			{'pin': 14, 'label': 'S021'},
+			{'pin': 15, 'label': 'S022'},
+			{'pin': 18, 'label': 'S023'},
+			{'pin': 10, 'label': 'S031'},
+			{'pin': 9, 'label': 'S032'},
+			{'pin': 11, 'label': 'S033'}
 		]
 
 		for pin_and_label in self.pin_and_label_matrix:
 			GPIO.setup(pin_and_label['pin'], GPIO.IN)
 			GPIO.add_event_detect(pin_and_label['pin'], GPIO.RISING, callback=self._on_data_received)
 			print("Pin " + str(pin_and_label['pin']) + " initialized as input.")
-		"""
-			Set action gpio to 0V
-		"""
-		GPIO.setmode(GPIO.BCM)
 
 		"""
 			Blink to show loaded
