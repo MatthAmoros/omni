818b7f93521fa8c6e7c938efdd913473fa710cdd
SunPaz <matth.amoros@gmail.com>
2019-09-09 08:51:57 -0700

    Added print output colors

--- lib/common.py
+++ lib/common.py
@@ -78,3 +78,13 @@ class ServerSetting:
 	def serialize(self):
 		"""Serializes current object instance (used with jsonify)"""
 		return { 'type': self.type, 'parameters' : self.parameters}
+
+class PrintColor:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
